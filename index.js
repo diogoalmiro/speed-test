c6477cbed165df15b59b58ba6ec3801fc9059264
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2025-11-20 19:29:37 +0000

    Files

---
+++ index.js
@@ -0,0 +1,65 @@
+const https = require("https");
+
+const Database = require("better-sqlite3");
+
+const db = new Database("speed.db");
+
+db.exec(`CREATE TABLE IF NOT EXISTS speed (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  start INTEGER,
+  end INTEGER,
+  duration INTEGER,
+  filesize INTEGER
+)`);
+
+const insertStm = db.prepare(`INSERT INTO speed
+    (start, end, duration, filesize) VALUES
+    (:start, :end, :duration, :filesize)`);
+
+const filesize = 1_000_000;
+const TEST_URL = "https://fsn1-speed.hetzner.com/100MB.bin";
+const TIMEOUT = 60_000;
+
+function downloadSpeed(url) {
+  return new Promise((resolve) => {
+    const start = Date.now();
+    let bytes = 0;
+
+    const req = https.get(url, { timeout: TIMEOUT }, (res) => {
+      res.on("data", chunk => {
+        bytes += chunk.length
+      });
+      res.on("end", () => {
+        resolve({
+            start,
+            end: Date.now(),
+            duration: Date.now() - start,
+            filesize: bytes,
+        });
+      });
+    });
+
+    req.on("error", () => resolve({
+        start,
+        end: Date.now(),
+        duration: Date.now() - start,
+        filesize: 0,
+    }));
+    req.on("timeout", () => {
+      req.destroy();
+      resolve({
+        start,
+        end: Date.now(),
+        duration: Date.now() - start,
+        filesize: 0,
+      });
+    });
+  });
+}
+
+async function runCheck() {
+  const speed = await downloadSpeed(TEST_URL);
+  insertStm.run(speed);
+}
+
+runCheck();
---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const { createReadStream } = require("fs");
+const Database = require("better-sqlite3");
+
+const db = new Database("speed.db");
+
+const selectStm = db.prepare(`SELECT start, end, duration, filesize, (filesize / duration) AS speed FROM speed ORDER BY id DESC LIMIT :limit OFFSET :offset`);
+const perPage = 100;
+
+http.createServer((req, res) => {
+    let url = new URL(req.url, "http://localhost");
+    try{
+    if( url.pathname === "/" || url.pathname === "/index.html") {
+        createReadStream("index.html").pipe(res);
+    }
+    else if( url.pathname === "/results") {
+        const page = parseInt(url.searchParams.get("page") || "1");
+        const results = selectStm.all({
+            limit: perPage,
+            offset: page * perPage,
+        });
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(results));
+    }
+    else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>404 Not Found</h1>");
+    }
+    } catch (error) {
+        console.error(url.pathname, error);
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("<h1>500 Internal Server Error</h1>");
+    }
+}).listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
